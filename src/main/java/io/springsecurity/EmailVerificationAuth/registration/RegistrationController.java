51e73935424751f5370b3af1ccdf3474fd9394b0
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-07-02 12:23:42 +0300

    feat: add registration file

---
+++ src/main/java/io/springsecurity/EmailVerificationAuth/registration/RegistrationController.java
@@ -0,0 +1,33 @@
+package io.springsecurity.EmailVerificationAuth.registration;
+
+import io.springsecurity.EmailVerificationAuth.event.RegistrationCompleteEvent;
+import io.springsecurity.EmailVerificationAuth.user.User;
+import io.springsecurity.EmailVerificationAuth.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/register")
+public class RegistrationController {
+    private final UserService userService;
+    private final ApplicationEventPublisher publisher;
+
+    @PostMapping
+    public String registerUser(RegistrationRequest registrationRequest, final HttpServletRequest request) {
+        User user = userService.registerUser(registrationRequest);
+        //publish registration event
+        publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
+
+        return "Success! Please, check your email for to complete your registration";
+    }
+
+    private String applicationUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+    }
+
+}
