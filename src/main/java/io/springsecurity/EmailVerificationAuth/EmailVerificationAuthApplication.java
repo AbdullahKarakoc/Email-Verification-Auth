d8bd18ee9acdcb31a97882255752816e376fcd1e
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-07-02 10:35:48 +0300

    first commit

---
+++ src/main/java/io/springsecurity/EmailVerificationAuth/EmailVerificationAuthApplication.java
@@ -0,0 +1,13 @@
+package io.springsecurity.EmailVerificationAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailVerificationAuthApplication.class, args);
+	}
+
+}
